ae6fef5eaf0ba80eb76484dad74c5dddeffe89d5
Dexter Ng <DNg@lcs.on.ca>
2024-01-09 20:23:00 -0500

    Question 5

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var greeting = "Hello, World!"
+
+struct Dog {
+    
+    let breed: String
+    let colour: String
+    var heightInMeters: Double
+    var heightInCentimeters: Double {
+        return heightInMeters * 100
+    }
+    var lengthInMeters: Double
+    var lengthInCentimeters: Double {
+        return lengthInMeters * 100
+    }
+    var weightInKilograms: Double
+    var weightInGrams: Double {
+        return weightInKilograms * 1000
+    }
+    var age: Int
+    let name: String
+    let birthday: String
+    
+}
